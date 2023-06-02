a9aa22421368dd1ad3c5f26d288abe0add4051ea
digify-integrated <digify.integrated@gmail.com>
2023-06-02 09:25:30 +0800

    To test generate OTP

--- test.php
+++ test.php
@@ -1,9 +1,11 @@
 <?php
     require('config/config.php');
-    require('classes/global-class.php');
+    require('model/database-model.php');
+    require('model/user-model.php');
 
-    $api = new Global_Class;
+    $api = new Database;
+    $user_api = new UserModel;
     #$api->backup_database('DB_'. date('m.d.Y'));
 
-    echo $api->encrypt_data("P@ssw0rd");
+    echo $user_api->generateOTP(6);
 ?>
