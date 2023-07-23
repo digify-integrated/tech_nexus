bab3596c4ded4e4e6a5dcc8f4dee512487c154d1
digify-integrated <digify.intergrated@gmail.com>
2023-07-23 12:23:06 +0800

    Finish role configuration.

--- view/_menu_group_details.php
+++ view/_menu_group_details.php
@@ -138,86 +138,86 @@
                         </div>
                       </div>
                     </div>';
-          }
 
-          if($menuItemCreateAccess['total'] > 0 || $menuItemWriteAccess['total'] > 0){
-            echo '<div id="menu-item-modal" class="modal fade modal-animate anim-fade-in-scale" tabindex="-1" role="dialog" aria-labelledby="modal-menu-item-modal" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="modal-menu-item-modal-title">Menu Item</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body" id="modal-body">
-                        <form id="menu-item-form" method="post" action="#">
-                        <div class="form-group">
-                            <label class="form-label" for="menu_item_name">Menu Item Name <span class="text-danger">*</span></label>
-                            <input type="hidden" id="menu_item_id" name="menu_item_id">
-                            <input type="text" class="form-control" id="menu_item_name" name="menu_item_name" maxlength="100" autocomplete="off">
-                        </div>
-                        <div class="form-group">
-                            <label class="form-label" for="menu_item_order_sequence">Order Sequence <span class="text-danger">*</span></label>
-                            <input type="number" class="form-control" id="menu_item_order_sequence" name="menu_item_order_sequence" min="0">
-                        </div>
-                        <div class="form-group">
-                            <label class="form-label" for="menu_item_url">URL</label>
-                            <input type="text" class="form-control" id="menu_item_url" name="menu_item_url" maxlength="50" autocomplete="off">
-                        </div>
-                        <div class="form-group">
-                            <label class="form-label" for="menu_item_icon">Menu Item Icon</label>
-                            <input type="text" class="form-control" id="menu_item_icon" name="menu_item_icon" maxlength="150" autocomplete="off">
-                        </div>
-                        <div class="form-group">
-                            <label class="form-label">Parent Menu Item</label>
-                            <select class="form-control modal-select2" name="parent_id" id="parent_id">
-                                <option value="">--</option>
-                                '. $menuItemModel->generateMenuItemOptions() .'
-                            </select>
-                        </div>
-                    </form>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" id="submit-menu-item-form" form="menu-item-form">Submit</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>';
-          }
-
-          if($updateMenuItemRoleAccess['total'] > 0){
-            echo '<div id="assign-menu-item-role-access-modal" class="modal fade modal-animate anim-fade-in-scale" tabindex="-1" role="dialog" aria-labelledby="modal-assign-menu-item-role-access-modal" aria-hidden="true">
-                  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg" role="document">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="modal-assign-menu-item-role-access-modal-title">Assign Menu Item Role Access</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body" id="modal-body"><form id="assign-menu-item-role-access-form" method="post" action="#">
-                      <div class="row">
-                          <div class="col-md-12">
-                              <table id="assign-menu-item-role-access-table" class="table table-striped table-hover table-bordered nowrap w-100 dataTable">
-                                  <thead>
-                                  <tr>
-                                      <th class="all">Role</th>
-                                      <th class="all">Read</th>
-                                      <th class="all">Write</th>
-                                      <th class="all">Create</th>
-                                      <th class="all">Delete</th>
-                                      <th class="all">Duplicate</th>
-                                  </tr>
-                                  </thead>
-                                  <tbody></tbody>
-                              </table>
-                          </div>
-                      </div>
-                  </form></div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" id="submit-menu-access-form" form="assign-menu-item-role-access-form">Submit</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>';
+                    if($menuItemCreateAccess['total'] > 0 || $menuItemWriteAccess['total'] > 0){
+                      echo '<div id="menu-item-modal" class="modal fade modal-animate anim-fade-in-scale" tabindex="-1" role="dialog" aria-labelledby="modal-menu-item-modal" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="modal-menu-item-modal-title">Menu Item</h5>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body" id="modal-body">
+                                  <form id="menu-item-form" method="post" action="#">
+                                  <div class="form-group">
+                                      <label class="form-label" for="menu_item_name">Menu Item Name <span class="text-danger">*</span></label>
+                                      <input type="hidden" id="menu_item_id" name="menu_item_id">
+                                      <input type="text" class="form-control" id="menu_item_name" name="menu_item_name" maxlength="100" autocomplete="off">
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="form-label" for="menu_item_order_sequence">Order Sequence <span class="text-danger">*</span></label>
+                                      <input type="number" class="form-control" id="menu_item_order_sequence" name="menu_item_order_sequence" min="0">
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="form-label" for="menu_item_url">URL</label>
+                                      <input type="text" class="form-control" id="menu_item_url" name="menu_item_url" maxlength="50" autocomplete="off">
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="form-label" for="menu_item_icon">Menu Item Icon</label>
+                                      <input type="text" class="form-control" id="menu_item_icon" name="menu_item_icon" maxlength="150" autocomplete="off">
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="form-label">Parent Menu Item</label>
+                                      <select class="form-control modal-select2" name="parent_id" id="parent_id">
+                                          <option value="">--</option>
+                                          '. $menuItemModel->generateMenuItemOptions() .'
+                                      </select>
+                                  </div>
+                              </form>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                  <button type="submit" class="btn btn-primary" id="submit-menu-item-form" form="menu-item-form">Submit</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>';
+                    }
+          
+                    if($updateMenuItemRoleAccess['total'] > 0){
+                      echo '<div id="assign-menu-item-role-access-modal" class="modal fade modal-animate anim-fade-in-scale" tabindex="-1" role="dialog" aria-labelledby="modal-assign-menu-item-role-access-modal" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title" id="modal-assign-menu-item-role-access-modal-title">Assign Menu Item Role Access</h5>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body" id="modal-body"><form id="assign-menu-item-role-access-form" method="post" action="#">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <table id="assign-menu-item-role-access-table" class="table table-striped table-hover table-bordered nowrap w-100 dataTable">
+                                            <thead>
+                                            <tr>
+                                                <th class="all">Role</th>
+                                                <th class="all">Read</th>
+                                                <th class="all">Write</th>
+                                                <th class="all">Create</th>
+                                                <th class="all">Delete</th>
+                                                <th class="all">Duplicate</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody></tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </form></div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                  <button type="submit" class="btn btn-primary" id="submit-menu-access-form" form="assign-menu-item-role-access-form">Submit</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>';
+                    }
           }
         ?>
